9c1ec0ca7055b2166b20c192b236fc007e7d43eb
dragonlobster <zai.keung@gmail.com>
2024-12-31 12:06:56 +0800

    first commit

---
+++ src/DrawUtils.cpp
@@ -0,0 +1,31 @@
+#include "DrawUtils.h"
+
+sf::RectangleShape DrawUtils::add_pixel(sf::Vector2f position, std::uint8_t red, std::uint8_t green, std::uint8_t blue) {
+	sf::RectangleShape pixel{};
+	pixel.setSize({ 1.f, 1.f });
+	pixel.setFillColor({ red, green, blue });
+
+	float x = position.x * DrawUtils::SCALE;
+	float y = position.y * DrawUtils::SCALE;
+
+	pixel.setPosition({ x, y });
+
+	// TODO: make scale a float
+	pixel.setScale({static_cast<float>(DrawUtils::SCALE), static_cast<float>(DrawUtils::SCALE)});
+	return pixel;
+
+}
+
+std::vector<sf::RectangleShape> DrawUtils::draw_display_buffer(const uint8_t(&display)[Chip8::DISPLAY_WIDTH * Chip8::DISPLAY_HEIGHT]) {
+	// TODO: see if there's a way to avoid redrawing everything
+	std::vector<sf::RectangleShape> pixels{};
+
+	for (unsigned int i = 0; i < sizeof(display); ++i) {
+		if (display[i] == 1) {
+			int x = i % Chip8::DISPLAY_WIDTH;
+			int y = i / Chip8::DISPLAY_WIDTH;
+			pixels.push_back(DrawUtils::add_pixel({ static_cast<float>(x), static_cast<float>(y) }, 255, 255, 255));
+		}
+	}
+	return pixels;
+}
